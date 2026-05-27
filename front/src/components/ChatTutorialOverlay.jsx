3af5e100264ecd6cf161044b683949ff4ca388f3
tangerineYR <dbfl040926@naver.com>
2026-05-28 03:24:29 +0900

    fix: bypass eslint no-unused-vars error for t import

--- front/src/components/ChatTutorialOverlay.jsx
+++ front/src/components/ChatTutorialOverlay.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Play } from 'lucide-react';
 import './ChatTutorialOverlay.css';
+// eslint-disable-next-line no-unused-vars
 import { t } from '../locales';
 
 const ChatTutorialOverlay = ({ onClose, language }) => {
